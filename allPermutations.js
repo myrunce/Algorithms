4665f2cd6288424882479be231eb1768a8d8fe6e
Myron Navarrete <myronnavarrete@gmail.com>
2017-10-22 22:14:35 -0700

    initial commit, added allPermutations

---
+++ allPermutations.js
@@ -0,0 +1,22 @@
+let allPerm = (str) => {
+    let perms = []
+    let perm = ''
+    
+   
+    let permutate = (word, holder) => {
+      if (!word) {
+        console.log(holder);
+        perms.push(holder);
+      }
+      for (let i = 0; i < word.length; i++) {
+        holder += word[i];
+        permutate(word.slice(0,i) + word.slice(i+1), holder);
+        holder = holder.slice(0, holder.length - 1)
+      }
+    }
+    
+    permutate(str, perm);
+    return perms;
+  }
+  
+  console.log(allPerm('ABC'));
